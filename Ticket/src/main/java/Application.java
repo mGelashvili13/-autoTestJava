2cce21f709f9b54ffb34ce95d54cb87196ec64d2
MaTokareva <mgelashvili@mail.ru>
2020-03-26 00:10:39 +0300

    создали билет

--- Ticket/src/main/java/Application.java
+++ Ticket/src/main/java/Application.java
@@ -1,10 +1,15 @@
 import model.Event;
-import model.FunctionalEnum;
+import model.Ticket;
 import model.User;
 
 import java.util.Scanner;
 
 public class Application {
+
+    private static User user;
+    private static Event event;
+    private static Ticket ticket;
+
     public static void main(String[] args) {
         System.out.println("Hello world");
 
@@ -13,22 +18,24 @@ public class Application {
         System.out.println("Введите номер операции: ");
         int num = in.nextInt();
         System.out.printf("Вы ввели: %d \n", num);
-            if (num == 1) {
-                System.out.println("создать пользователя");
-                User someUser = new User(1, "Will Jones", "34324234");
-                System.out.println("создали пользователя "+someUser.name);
-            }else {
-                System.out.println("число не 1");
-            }
-            if (num == 2){
-                System.out.println("создать событие ");
-                Event someEvent= new Event(11, "новогодняя ярморка" );
-                System.out.println("создать событие "+ someEvent.titleEvent);
-            }if (num==3){
+        if (num == 1) {
+            System.out.println("создать пользователя");
+            User someUser = new User(1, "Will Jones", "34324234");
+            user=someUser;
+            System.out.println("создали пользователя " + someUser.name);
+        }
+        if (num == 2) {
+            System.out.println("создать событие ");
+            Event someEvent = new Event(11, "новогодняя ярморка");
+            event=someEvent;
+            System.out.println("создать событие " + someEvent.titleEvent);
+        }
+        if (num == 3) {
             System.out.println("создать билет для пользователя на событие");
-            User someUser = new User(19, "Вася Пупкин", "77777");
-            Event someEvent= new Event(11, "новогодняя фрморка");
-            System.out.println("создать билет на пользователя "+someUser.name +" на мероприятие "+someEvent.titleEvent );
+           /* User someUser = new User(19, "Вася Пупкин", "77777");
+            Event someEvent = new Event(11, "новогодняя ярморка");*/1
+           ticket=new Ticket(7,event.idEvent,user.id,56);
+            System.out.println("создать билет на пользователя " + user.name + " на мероприятие " + event.titleEvent);
         }
 
 
