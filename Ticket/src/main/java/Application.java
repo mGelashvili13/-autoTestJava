15bc233abb4af9ea40ed9e980e5467bf4e2cf0b4
MaTokareva <mgelashvili@mail.ru>
2020-03-26 00:46:34 +0300

    добавили цикл для ввода номера операции while

--- Ticket/src/main/java/Application.java
+++ Ticket/src/main/java/Application.java
@@ -15,37 +15,36 @@ public class Application {
 
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите номер операции: ");
-        int num = in.nextInt();
-        System.out.printf("Вы ввели: %d \n", num);
-        switch (num) {
 
-            case 1:
-                System.out.println("создать пользователя");
-                User someUser = new User(1, "Will Jones", "34324234");
-                user = someUser;
-                System.out.println("создали пользователя " + someUser.name);
+        while (true) {
+            System.out.println("Введите номер операции: ");
+            int num = in.nextInt();
+            System.out.printf("Вы ввели: %d \n", num);
 
-            case 2:
-                System.out.println("создать событие ");
-                Event someEvent = new Event(11, "new year fair");
-                event = someEvent;
-                System.out.println("создать событие " + someEvent.titleEvent);
+            switch (num) {
 
-            case 3:
-                System.out.println("создать билет для пользователя на событие");
-                if (!(event == null || user == null)) {
-                    ticket = new Ticket(7, event.idEvent, user.id, 56);
-                    System.out.println("создать билет на пользователя " + user.name + " на мероприятие " + event.titleEvent);
-
-                } else {
-                    System.out.println("создать билет невозможно");
-
-
-                }
-                break;
+                case 1:
+                    System.out.println("создать пользователя");
+                    User someUser = new User(1, "Will Jones", "34324234");
+                    user = someUser;
+                    System.out.println("создали пользователя " + someUser.name);
+                    break;
+                case 2:
+                    System.out.println("создать событие ");
+                    Event someEvent = new Event(11, "new year fair");
+                    event = someEvent;
+                    System.out.println("создали событие " + someEvent.titleEvent);
+                    break;
+                case 3:
+                    System.out.println("создать билет для пользователя на событие");
+                    if (!(event == null || user == null)) {
+                        ticket = new Ticket(7, event.idEvent, user.id, 56);
+                        System.out.println("создали билет на пользователя " + user.name + " на мероприятие " + event.titleEvent);
+                    } else {
+                        System.out.println("создать билет невозможно");
+                    }
+                    break;
+            }
         }
-
-
     }
 }
